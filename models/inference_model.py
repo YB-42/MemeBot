deafe11896102749b581513d69b8b9fe921b9205
Yan <blondthelit@gmail.com>
2025-07-16 14:52:36 +0300

    delete hardcode fields in code

--- models/inference_model.py
+++ models/inference_model.py
@@ -4,12 +4,27 @@ from PIL import Image
 from resnet import ResNet
 from vit import VisionTransformer
 
-model = EfficientNet()
 
-model.load("data/EffNet.pth")
+def run_inference(model_name: str, image: Image.Image) -> torch.Tensor:
+    if model_name == "EfficientNet":
+        model = EfficientNet()
+        model.load("data/EffNet.pth")
+    elif model_name == "ResNet":
+        model = ResNet()
+        model.load("data/ResNet.pth")
+    elif model_name == "ViT":
+        model = VisionTransformer()
+        model.load("data/ViT.pth")
+    else:
+        raise ValueError("Invalid model name")
 
-image = Image.open("image.jpg").convert("RGB")
+    image_filt = Image.open("image.jpg").convert("RGB")
+    prediction = model.predict(image_filt)
 
-prediction = model.predict(image)
+    return prediction
 
-print("Prediction", prediction)
+
+model_n = input()
+image = Image.open("image.jpg")
+print(f"Prediction: {run_inference(model_n, image)}")
+
